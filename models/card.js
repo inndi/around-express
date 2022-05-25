0b65ace739adc57977d46b370a71f7d9beb29f11
Inna <inndi.spivakova@gmail.com>
2022-05-25 19:24:21 +0300

    feat: create the models folder, the user and card files; add a schema for user

---
+++ models/card.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const cardSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^https?:\/\/[\w.]*[\w\W]+\.[\w\W]+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid address!`
+    },
+  }
+})
