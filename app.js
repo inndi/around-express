e71531cfea45e7e3d8ea874d4b4b92839c942a39
Inna <inndi.spivakova@gmail.com>
2022-05-25 13:23:33 +0300

    feat: connect to MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 const app = express();
 app.use(helmet());
