96d7c1c2a09af048d3084ae0c44b84be5ce47576
Inna <inndi.spivakova@gmail.com>
2022-05-28 13:52:47 +0300

    feat: add an update request handler to user

--- controllers/users.js
+++ controllers/users.js
@@ -45,3 +45,46 @@ module.exports.createUser = (req, res) => {
       };
     });
 };
+
+module.exports.updateUser = (req, res) => {
+  const { name, about } = req.body;
+  const { id } = req.params;
+
+  User.findByIdAndUpdate(id, { name, about }, {
+    new: true,
+    runValidators: true,
+    upsert: true
+  })
+    .then(user => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).send({ message: 'invalid data passed to the methods for updating a user' });
+      } else if (err.name === "CastError") {
+        res.status(404).send({ message: 'user not found' });
+      } else {
+        res.status(500).send({ message: 'An error has occurred' });
+      };
+    });
+}
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const { id } = req.params;
+
+  User.findByIdAndUpdate(id, { avatar },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true
+    })
+    .then(user => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).send({ message: 'invalid data passed to the methods for updating a user' });
+      } else if (err.name === "CastError") {
+        res.status(404).send({ message: 'user not found' });
+      } else {
+        res.status(500).send({ message: `An error ${err.name} has occurred` });
+      };
+    });
+}
